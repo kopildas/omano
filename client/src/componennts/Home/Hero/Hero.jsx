41f4faff89c4e2216845b65230628ae0ad6ac41f
kopildas <102140363+kopildas@users.noreply.github.com>
2023-12-28 14:36:05 +0600

    feat: hero picture moveability

--- client/src/componennts/Home/Hero/Hero.jsx
+++ client/src/componennts/Home/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Icon_Magnetic from './Icon_Magnetic'
 
 export default function Hero() {
   return (
@@ -18,7 +19,9 @@ export default function Hero() {
 
               </div>
             </div>
+            <Icon_Magnetic>
             <img src="https://i.ibb.co/LNmBY5J/one-and-only-rectangular-plate-of-food-vegetable-omlate-chicken-hyper-realistic-4k-smooth-pro-490600.png" alt="one-and-only-rectangular-plate-of-food-vegetable-omlate-chicken-hyper-realistic-4k-smooth-pro-490600" border="0" className='w-80 -ml-10 mt-14 z-20'/>
+            </Icon_Magnetic>
             <div className='w-1/2  bg-red-30 flex items- justify- absolute z-10 mt-[16rem]'>
               <div className='w-24 h-24 rounded-blob-1 bg-blob-color2 rotate-45 ml-10 '>
 
