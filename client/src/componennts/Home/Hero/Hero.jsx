2809f75f3c26988663603e82a8b0b9f97e5320cc
kopildas <102140363+kopildas@users.noreply.github.com>
2023-12-30 19:35:06 +0600

    fix: hero section pic adjustments

--- client/src/componennts/Home/Hero/Hero.jsx
+++ client/src/componennts/Home/Hero/Hero.jsx
@@ -20,7 +20,7 @@ export default function Hero() {
               </div>
             </div>
             <Icon_Magnetic>
-            <img src="https://i.ibb.co/LNmBY5J/one-and-only-rectangular-plate-of-food-vegetable-omlate-chicken-hyper-realistic-4k-smooth-pro-490600.png" alt="one-and-only-rectangular-plate-of-food-vegetable-omlate-chicken-hyper-realistic-4k-smooth-pro-490600" border="0" className='w-80 -ml-10 mt-14 z-20'/>
+            <img src="https://i.ibb.co/LNmBY5J/one-and-only-rectangular-plate-of-food-vegetable-omlate-chicken-hyper-realistic-4k-smooth-pro-490600.png" alt="one-and-only-rectangular-plate-of-food-vegetable-omlate-chicken-hyper-realistic-4k-smooth-pro-490600" border="0" className='w-80 -ml-5 mt-14 z-20'/>
             </Icon_Magnetic>
             <div className='w-1/2  bg-red-30 flex items- justify- absolute z-10 mt-[16rem]'>
               <div className='w-24 h-24 rounded-blob-1 bg-blob-color2 rotate-45 ml-10 '>
