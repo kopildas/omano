2bdadf93956ad03758061c28efad9785b1088e92
kopildas <102140363+kopildas@users.noreply.github.com>
2023-12-30 18:37:01 +0600

    feat: No review yet text showed

--- client/src/pages/Shop/Review.jsx
+++ client/src/pages/Shop/Review.jsx
@@ -41,7 +41,7 @@ console.log(reviews);
   return (
     <div>
       <div>
-        {reviews &&
+        {reviews.length>0 ?
           reviews.map((item) => (
             <div key={item._id} className="gap-2 p-2 w-full bg-red-00" >
               <div className="flex flex-row gap-2 bg-gray-20 px-2 py-1">
@@ -50,15 +50,21 @@ console.log(reviews);
                 </div>
                 <div className="text-sm w-full bg-orange-00">
               <div className="flex items-start justify-between w-full">
-              <p className="font font-semibold">{item.user_name || `User`}</p>
+              <p className="font font-semibold text-gray-800">{item.user_name || `User`}</p>
               <ReactStar size={15} value={item.rating}  />
               </div>
-              <p>{item.review}</p>
+              <p className="text-gray-600">{item.review}</p>
                 </div>
               </div>
               
             </div>
-          ))}
+          )) : (
+            <div className="flex flex-col mt-10 text-gray-400 items-center justify-center ">
+                <p>No review yet,</p>
+                <p>Please leave a review</p>
+                <p>become the first.</p>
+            </div>
+          )}
       </div>
     </div>
   );
